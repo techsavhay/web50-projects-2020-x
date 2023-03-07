5b05af912c4e1825d8577b9cee0ff0077ca69d7f
treasurebum <ianandrewhay@gmail.com>
2023-03-07 14:03:09 +0000

    url basic wiki lookup working

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
-    path("wiki/>str:title>", views.entry, name="entry"),
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.entry, name="entry"),
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -8,16 +8,16 @@ def index(request):
         "entries": util.list_entries()
     })
 
-def entry(request, title)
-        content = util.get_entry(title)
+def entry(request, title):
+    content = util.get_entry(title)
         
-        if content is None:
-            return render(request, encyclopedia/error.html, {
-                 "title:title"
-            })
+    if content is None:
+        return render(request, "encyclopedia/error.html", {
+            "title": title
+        })
                            
-        else:
-            return render(request, encyclopedia/entry.html, {
-                 "title":title,
-                 "content":content
-            })
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": content
+        })
