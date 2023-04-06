8c6c36171836fb20c68d354c477cf56f982bc568
treasurebum <ianandrewhay@gmail.com>
2023-04-06 11:40:12 +0100

    small headway made on send email JS

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -31,3 +31,51 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 }
+
+function send_email() {
+  // Selects the submit button to be used later.
+  const submit = document.querySelector('submit');
+  const compose_recipients  = document.querySelector('#compose-recipients');
+
+  // Disable submit button by default so it doesnt submit blank emails:
+  submit.disabled = true;
+
+  // Listen for input to be typed into the compose_recipients input field
+  compose_recipients.onkeyup = () => {
+    if (compose_recipients.value.length > 0) {
+        submit.disabled = false;
+    }
+    else {
+        submit.disabled = true;
+    }
+}
+
+  // Listen for submission of the form on the compose page
+  document.querySelector('form').onsubmit = () => {
+
+    
+    // get the value of the recipient, subject and body fields
+    const recipients = compose_recipients.value;
+    const subject = document.querySelector('#compose-subject').value;
+    const body = document.querySelector('#compose-body').value;
+
+    
+    obj[recipients]
+
+    // POST the email
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+          recipients: 'recipients',
+          subject: 'subject',
+          body: 'body'
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+        // Print result
+        console.log(result);
+    });
+  }
+
+}
