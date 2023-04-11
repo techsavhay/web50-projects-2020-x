6e32fd931cb1a8c0aa5062b77bd8eb3bd144b55f
treasurebum <ianandrewhay@gmail.com>
2023-04-11 15:28:36 +0100

    added start of mailbox code tbf

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -1,4 +1,7 @@
+console.log("Inbox.js loaded");
+
 document.addEventListener('DOMContentLoaded', function() {
+  console.log('DOMContentLoaded event triggered'); 
 
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
@@ -9,33 +12,63 @@ document.addEventListener('DOMContentLoaded', function() {
   // By default, load the inbox
   load_mailbox('inbox');
 
+  // Add submit event listener to the compose form
+  document.querySelector('#compose-form').addEventListener('submit', function(event) {
+    // Prevent default submission
+    event.preventDefault();
 
-  function compose_email() {
-    // Show compose view and hide other views
-    document.querySelector('#emails-view').style.display = 'none';
-    document.querySelector('#compose-view').style.display = 'block';
-  
-    // Clear out composition fields
-    document.querySelector('#compose-recipients').value = '';
-    document.querySelector('#compose-subject').value = '';
-    document.querySelector('#compose-body').value = '';
-  
-    // Attach the send_email function to the form submit event
-    document.querySelector('form').onsubmit = send_email;
-  }
-  
+    // Send email
+    send_email();
+
+    console.log('Submit event triggered');
+  });
+
+});
+
+function compose_email() {
+  console.log('compose_email function called'); 
+
+  // Show compose view and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'block';
+
+  // Clear out composition fields
+  document.querySelector('#compose-recipients').value = '';
+  document.querySelector('#compose-subject').value = '';
+  document.querySelector('#compose-body').value = '';
+}
+
 
 function load_mailbox(mailbox) {
-  
+  console.log(`load_mailbox function called with mailbox: ${mailbox}`); 
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // api request to get the emails from the relevent mailbox
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+    // Print emails
+    console.log(emails)
+
+    // ... do something else with emails ...
+    for (email in emails);
+    pass
+
+
+
+});
 }
 
+
 function send_email() {
+  console.log('send_email function called'); 
+
   // Selects the submit button to be used later.
   const submit = document.querySelector('input[type="submit"]');
   const compose_recipients = document.querySelector('#compose-recipients');
@@ -53,59 +86,47 @@ function send_email() {
     }
   }
 
-  // Listen for submission of the form on the compose page, if submit is pressed run the following code
-  document.querySelector('form').onsubmit = (event) => {
-    event.preventDefault();
-    console.log('Form was submitted'); 
+  // Remove the previous event listener and add a new one
+  document.querySelector('#compose-form').removeEventListener('submit', form_submit_handler);
+  document.querySelector('#compose-form').addEventListener('submit', form_submit_handler);
 
-    // set field name variables which will be used later as keys in key:value pairs.
+  function form_submit_handler(event) {
+    console.log('Form submit handler called'); // New console log
+    event.preventDefault();
+
     var recipients = "recipients";
     var subject = "subject";
     var body = "body";
 
-    // create javascript object
     var obj = {};
-
-    // set object variable key : value from the previously created keys and values taken from the form fields.
     obj[recipients] = compose_recipients.value;
     obj[subject] = document.querySelector('#compose-subject').value;
     obj[body] = document.querySelector('#compose-body').value;
 
-    // POST the email
     fetch('/emails', {
       method: 'POST',
       body: JSON.stringify(obj)
     })
     .then(response => {
       if (!response.ok) {
-        // If the response is not okay, throw the response to be caught in the catch block
         throw response;
       }
       return response.json();
     })
     .then(result => {
-      // Print result
       console.log(result.message);
-      console.log('Email sent successfully'); // debug console statement
-      //if no errors come back load the sent mailbox function TO BE CODED!!!
+      console.log('Email sent successfully');
       load_mailbox('sent');
     })
     .catch(error => {
       if (error instanceof Response) {
         error.json().then(err => {
           console.error('Error:', err.error);
-          // Show an error message to the user
         });
       } else {
         console.error('Error:', error.message);
-        // Show a generic error message to the user
       }
     });
   }
 }
 
-
-//def 
-
-
-});
