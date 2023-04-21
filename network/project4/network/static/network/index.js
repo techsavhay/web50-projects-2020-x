597eda02ab48c5cd20d4dd14706ede3c55e8a30a
treasurebum <ianandrewhay@gmail.com>
2023-04-21 11:10:06 +0100

    added index.js to git

---
+++ network/project4/network/static/network/index.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Add event listener for the newpost form submission
+    document.querySelector('#newpost-form').addEventListener('submit', (event) => {
+        event.preventDefault();
+        const content = document.querySelector('#newpost-textarea').value.trim();
+
+        // Token for security
+        const csrfToken = document.getElementsByName("csrfmiddlewaretoken")[0].value;
+
+        fetch('/api/save_post', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrfToken // Must include the CSRF token in the headers
+            },
+            body: JSON.stringify({
+                content: content
+            })
+        })
+            .then(response => {
+                if (response.ok) {
+                    // If the request was successful, reload the page
+                    location.reload();
+                } else {
+                    // If there is an error, show the error message in the console
+                    console.error('Error saving post:', response.statusText);
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    });
+});
