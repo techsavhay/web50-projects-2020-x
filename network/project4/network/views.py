13e8034d752b819c4e87d5dd40e8291c07ef0b8b
treasurebum <ianandrewhay@gmail.com>
2023-04-21 10:07:42 +0100

    finished basic new post function

--- network/project4/network/views.py
+++ network/project4/network/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
-
+from django.contrib.auth.models import UserManager
 from .models import User, Post, Like
-
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 def index(request):
     return render(request, "network/index.html")
@@ -51,7 +52,7 @@ def register(request):
 
         # Attempt to create new user
         try:
-            user = User.objects.create_user(username, email, password)
+            user = User.objects.create_user(username, email, password) # type: ignore
             user.save()
         except IntegrityError:
             return render(request, "network/register.html", {
@@ -62,6 +63,20 @@ def register(request):
     else:
         return render(request, "network/register.html")
     
+@csrf_exempt
 def save_post(request):
-    post = 
+    if request.method == 'POST':
+        # Load the JSON data from request.body
+        data = json.loads(request.body)
 
+        # Get the content from the data dictionary
+        content = data.get('content', '').strip()
+
+        if content:
+            new_post = Post(content=content, post_owner=request.user)
+            new_post.save()
+            return JsonResponse({"success": True})
+        else:
+            return JsonResponse({"success": False, "error": "Content is empty."})
+    else:
+        return JsonResponse({"success": False, "error": "Invalid request method."})
