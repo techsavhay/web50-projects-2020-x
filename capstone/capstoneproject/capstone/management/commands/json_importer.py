5d227f77a22d868618e49bc4508826f1476d83c7
treasurebum <ianandrewhay@gmail.com>
2023-06-07 16:38:52 +0100

    tweak to star mapping in json importer

--- capstone/capstoneproject/capstone/management/commands/json_importer.py
+++ capstone/capstoneproject/capstone/management/commands/json_importer.py
@@ -16,41 +16,51 @@ def handle(self, *args, **options):
         pubs_json = json.load(f)
 
     for pub_data in pubs_json:
-        # Extract the relevant data from the item
-        name = pub_data["Pub Name"]
-        address = pub_data["Address"]
-        description = pub_data["Description"]
-        heritage_stars = pub_data["Inventory Stars"]
-        listed = pub_data["Listed"]
-        status = pub_data["Status"]
-        url = pub_data["Url"]
+        try:
+            # Extract the relevant data from the item
+            name = pub_data["Pub Name"]
+            address = pub_data["Address"]
+            description = pub_data["Description"]
+            heritage_stars = pub_data["Inventory Stars"]
+            listed = pub_data["Listed"]
+            status = pub_data["Status"]
+            url = pub_data["Url"]
 
-        star_mapping = {
-            "Three star": "3",
-            "Two star": "2",
-            "One star": "1",
-        }
+            star_mapping = {
+                "Three star": "3",
+                "Two star": "2",
+                "One star": "1",
+                "Zero star": "0",
+            }
 
-        # Convert heritage_stars into integers
-        if heritage_stars.startswith(tuple(star_mapping.keys())):
-            heritage_stars = star_mapping[heritage_stars]
-        else:
-            heritage_stars = None
+            # Convert heritage_stars into integers
+            if heritage_stars.startswith(tuple(star_mapping.keys())):
+                heritage_stars = star_mapping[heritage_stars]
+            else:
+                heritage_stars = None
 
-        # Check the status field and convert it to boolean
-        is_open = False if status and status != "" else True
+            # Check the status field and convert it to boolean
+            is_open = False if status and status != "" else True
 
-        # Create a new instance of the model
-        pub = TEST_Pub(
-            name=name,
-            address=address,
-            description=description,
-            inventory_stars=heritage_stars,
-            listed=listed,
-            open=is_open,
-            url=url
-        )
-        pub.save()
+            # Create a new instance of the model
+            pub = TEST_Pub(
+                name=name,
+                address=address,
+                description=description,
+                inventory_stars=heritage_stars,
+                listed=listed,
+                open=is_open,
+                url=url
+            )
+            pub.save()
+        
+        except KeyError as e:
+            # Handle missing or empty fields
+            self.stdout.write(self.style.WARNING(f"Missing or empty field in JSON: {str(e)}"))
+
+        except Exception as e:
+            # Handle other exceptions
+            self.stdout.write(self.style.ERROR(f"An error occurred: {str(e)}"))
 
 
         # Display a success message
