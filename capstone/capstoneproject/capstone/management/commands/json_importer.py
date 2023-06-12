1fba675f5108f116e727c1135b8a6aaeb7ff01db
treasurebum <ianandrewhay@gmail.com>
2023-06-12 11:51:00 +0100

    added database name argument to json importer

--- capstone/capstoneproject/capstone/management/commands/json_importer.py
+++ capstone/capstoneproject/capstone/management/commands/json_importer.py
@@ -7,62 +7,61 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('file', type=str, help='Path to the JSON file')
+        parser.add_argument('--database', type=str, default='default', help='Database name')
 
-def handle(self, *args, **options):
-    file_path = options['file']
+    def handle(self, *args, **options):
+        file_path = options['file']
 
-    # Open the JSON file and load its contents
-    with open(file_path) as f:
-        pubs_json = json.load(f)
+        # Open the JSON file and load its contents
+        with open(file_path) as f:
+            pubs_json = json.load(f)
 
-    for pub_data in pubs_json:
-        try:
-            # Extract the relevant data from the item
-            name = pub_data["Pub Name"]
-            address = pub_data["Address"]
-            description = pub_data["Description"]
-            heritage_stars = pub_data["Inventory Stars"]
-            listed = pub_data["Listed"]
-            status = pub_data["Status"]
-            url = pub_data["Url"]
+        for pub_data in pubs_json:
+            try:
+                # Extract the relevant data from the item
+                name = pub_data["Pub Name"]
+                address = pub_data["Address"]
+                description = pub_data["Description"]
+                heritage_stars = pub_data["Inventory Stars"]
+                listed = pub_data["Listed"]
+                status = pub_data["Status"]
+                url = pub_data["Url"]
 
-            star_mapping = {
-                "Three star": "3",
-                "Two star": "2",
-                "One star": "1",
-                "Zero star": "0",
-            }
+                star_mapping = {
+                    "Three star": "3",
+                    "Two star": "2",
+                    "One star": "1",
+                    "Zero star": "0",
+                }
 
-            # Convert heritage_stars into integers
-            if heritage_stars.startswith(tuple(star_mapping.keys())):
-                heritage_stars = star_mapping[heritage_stars]
-            else:
-                heritage_stars = None
+                # Convert heritage_stars into integers
+                if heritage_stars.startswith(tuple(star_mapping.keys())):
+                    heritage_stars = star_mapping[heritage_stars]
+                else:
+                    heritage_stars = None
 
-            # Check the status field and convert it to boolean
-            is_open = False if status and status != "" else True
+                # Check the status field and convert it to boolean
+                is_open = False if status and status != "" else True
 
-            # Create a new instance of the model
-            pub = TEST_Pub(
-                name=name,
-                address=address,
-                description=description,
-                inventory_stars=heritage_stars,
-                listed=listed,
-                open=is_open,
-                url=url
-            )
-            pub.save()
-        
-        except KeyError as e:
-            # Handle missing or empty fields
-            #self.stdout.write(self.style.WARNING(f"Missing or empty field in JSON: {str(e)}"))
-            pass
+                # Create a new instance of the model
+                pub = TEST_Pub(
+                    name=name,
+                    address=address,
+                    description=description,
+                    inventory_stars=heritage_stars,
+                    listed=listed,
+                    open=is_open,
+                    url=url
+                )
+                pub.save()
 
-        except Exception as e:
-            # Handle other exceptions
-            self.stdout.write(self.style.ERROR(f"An error occurred: {str(e)}"))
+            except KeyError as e:
+                # Handle missing or empty fields
+                pass
 
+            except Exception as e:
+                # Handle other exceptions
+                self.stdout.write(self.style.ERROR(f"An error occurred: {str(e)}"))
 
         # Display a success message
         self.stdout.write(self.style.SUCCESS('Data imported successfully'))
