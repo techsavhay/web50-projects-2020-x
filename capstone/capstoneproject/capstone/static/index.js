28cc553939f998a594cc16925833b450f8fd35ec
treasurebum <ianandrewhay@gmail.com>
2023-06-26 16:06:48 +0100

    lots of css and index.js work to get pubs to expand and contract on click. sort of working but with navbar issues

--- capstone/capstoneproject/capstone/static/index.js
+++ capstone/capstoneproject/capstone/static/index.js
@@ -1,57 +1,69 @@
 function fetchPubData() {
   fetch('/api/pubs/', {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
-      "X-CSRFToken": document.querySelector('[name=csrfmiddlewaretoken]').value,
+      'Content-Type': 'application/json',
+      'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value,
     },
     body: JSON.stringify({}),
   })
-  .then(response => response.json())
-  .then(data => {
+    .then(response => response.json())
+    .then(data => {
+      // Sort pubs alphabetically by name
+      sorted_pubs = data.sort(function(a, b) {
+        if (a.pub.name < b.pub.name) {
+          return -1;
+        }
+        if (a.pub.name > b.pub.name) {
+          return 1;
+        }
+        return 0;
+      });
 
-    // logs the response data in console
-    //console.log(data);
+      // Process data and update HTML elements
+      const pubsContainer = document.querySelector('#pubs-container');
+      pubsContainer.innerHTML = '';
 
-    // sorts pubs alphabetically by name
-    sorted_pubs = data.sort(function (a, b) {
-      if(a.pub.name < b.pub.name) {
-        return -1;
-      }
-      if(a.name > b.name) {
-        return 1;
-      }
-      return 0;
+      sorted_pubs.forEach(item => {
+        const pub = item.pub;
+        // Access pub properties
+        const name = pub.name;
+        const address = pub.address;
+
+        const pubElement = document.createElement('div');
+        pubElement.classList.add('pub');
+
+        pubElement.innerHTML = `
+          <h5>${name}</h5>
+          <p class="pub-address">${address}</p>
+        `;
+
+        // Event listener to pub element
+        pubElement.addEventListener('click', () => {
+          pubElement.classList.toggle('pub-expanded');
+          pubsContainer.style.height = 'auto'; // Reset the container height
+
+          // Delay the height calculation so CSS transition takes effect
+          setTimeout(() => {
+            const containerHeight = pubsContainer.offsetHeight;
+            pubsContainer.style.height = `${containerHeight}px`;
+
+            if (pubElement.classList.contains('pub-expanded')) {
+              const expandedPubHeight = pubElement.offsetHeight * 2;
+              pubElement.style.height = `${expandedPubHeight}px`;
+            } else {
+              pubElement.style.height = 'auto';
+            }
+          }, 0);
+        });
+
+        pubsContainer.appendChild(pubElement);
+      });
     })
-
-    // logs the response data in console
-    console.log(sorted_pubs);
-
-    // process data and update html elements
-    
-    const pubsContainer = document.querySelector("#pubs-container");
-    pubsContainer.innerHTML = '';
-
-    sorted_pubs.forEach(item => {
-      const pub = item.pub;
-      // Access pub properties
-      const name = pub.name;
-      const address = pub.address;
-
-      const pubElement = document.createElement("div");
-      pubElement.classList.add("pub");
-    
-      pubElement.innerHTML = `
-        <h5>${name}</h5>
-        <p class="pub-address">${address}</p>
-      `;
-      pubsContainer.appendChild(pubElement);
+    .catch(error => {
+      // log errors
+      console.error('Error fetching data:', error);
     });
-  })
-  .catch(error => {
-    // handle errors that occur by telling console
-    console.error('Error fetching data:', error);
-  });
 }
 
 fetchPubData();
