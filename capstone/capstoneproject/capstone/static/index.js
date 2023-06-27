3ebcd0ef950e1dbb9261cc82e2d856be60735a0f
treasurebum <ianandrewhay@gmail.com>
2023-06-27 11:38:08 +0100

    adjusted text area wight and save visit button positioning. tweaked code formatting and comments. click function working well.

--- capstone/capstoneproject/capstone/static/index.js
+++ capstone/capstoneproject/capstone/static/index.js
@@ -10,7 +10,7 @@ function fetchPubData() {
     .then(response => response.json())
     .then(data => {
       // Sort pubs alphabetically by name
-      sorted_pubs = data.sort(function(a, b) {
+      const sorted_pubs = data.sort((a, b) => {
         if (a.pub.name < b.pub.name) {
           return -1;
         }
@@ -38,28 +38,29 @@ function fetchPubData() {
           <p class="pub-address">${address}</p>
         `;
 
-        // Event listener to pub element
+        // Event listener for pub element
         pubElement.addEventListener('click', event => {
           const clickedElement = event.target;
           const clickedParent = clickedElement.parentElement;
 
-          if (//ignore the input fields when looking for a pub click//
+          // Ignore clicks on additional content or its parent elements
+          if (
             !clickedElement.classList.contains('additional-content') &&
             !clickedParent.classList.contains('additional-content')
           ) {
             pubElement.classList.toggle('pub-expanded');
 
-            // Delay the height calc so CSS transition takes effect
+            // Delay the height calculation so CSS transition takes effect
             setTimeout(() => {
               const containerHeight = pubsContainer.offsetHeight;
               pubsContainer.style.height = `${containerHeight}px`;
 
               if (pubElement.classList.contains('pub-expanded')) {
-                const expandedPubHeight = pubElement.offsetHeight * 3;
+                const expandedPubHeight = pubElement.offsetHeight * 4;
                 pubElement.style.height = `${expandedPubHeight}px`;
                 pubElement.insertAdjacentHTML(
                   'beforeend',
-                  '<form class="additional-content">Date of visit: <input type="date" name="visit"/><br><textarea id="pubreview" name="pubreview" rows="3" cols="30">Space to write a short review (optional)</textarea><input type="submit" value="Save visit"></form>'
+                  '<form class="additional-content">Date of visit: <input type="date" name="visit"/><br><textarea id="pubreview" name="pubreview" rows="3" cols="30" maxlength="280">Space to write a short review (optional)</textarea><input type="submit" value="Save visit"></form>'
                 );
               } else {
                 pubElement.style.height = 'auto';
@@ -67,6 +68,22 @@ function fetchPubData() {
                 if (additionalContent) {
                   additionalContent.remove();
                 }
+              }
+
+              // Set textarea width as a percentage of pubElement
+              const additionalContent = pubElement.querySelector('.additional-content');
+              if (additionalContent) {
+                const pubWidth = pubElement.offsetWidth; // Get the width of the pub element
+                const pubHeight = pubElement.offsetHeight; // Get the height of the pub element
+                const textarea = additionalContent.querySelector('textarea');
+
+                // Set the width of the textarea as a percentage of the pub width
+                const textareaWidthPercentage = 90; // Adjustable
+                const textareaHeightPercentage = 35; // Adjustable
+                const textareaWidth = (pubWidth * textareaWidthPercentage) / 100;
+                const textareaHeight = (pubHeight * textareaHeightPercentage) / 100;
+                textarea.style.width = `${textareaWidth}px`;
+                textarea.style.height = `${textareaHeight}px`;
               }
             }, 0);
           }
@@ -76,7 +93,7 @@ function fetchPubData() {
       });
     })
     .catch(error => {
-      // log errors
+      // Log errors
       console.error('Error fetching data:', error);
     });
 }
