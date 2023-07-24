bc14ba3e8c694445446bda071328b4575ea1a8f0
treasurebum <ianandrewhay@gmail.com>
2023-07-24 12:28:00 +0100

    added ability to search for pubs that x user has visited in admin area

--- capstone/capstoneproject/capstone/admin.py
+++ capstone/capstoneproject/capstone/admin.py
@@ -3,17 +3,22 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import Pub, Post
 
-
 User = get_user_model()
 
 admin.site.unregister(User)
 admin.site.register(User, BaseUserAdmin)
 
-@admin.register(Pub)
 class PubAdmin(admin.ModelAdmin):
     list_display = ['name', 'address', 'inventory_stars', 'open']
     list_filter = ['inventory_stars', 'open']
-    search_fields = ['name', 'address',]
+    search_fields = ['name', 'address', 'users_visited__username'] 
+
+    # override get_search_results method
+    def get_search_results(self, request, queryset, search_term):
+        queryset, use_distinct = super().get_search_results(request, queryset, search_term)
+        if search_term:
+            queryset |= self.model.objects.filter(users_visited__username__icontains=search_term)
+        return queryset, use_distinct
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -24,3 +29,6 @@ class PostAdmin(admin.ModelAdmin):
     def get_pub_name(self, obj):
         return obj.pub.name
     get_pub_name.short_description = 'Pub Name' 
+
+# update register call for PubAdmin
+admin.site.register(Pub, PubAdmin)
